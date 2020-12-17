70ea24f0afed82aa97c3525b2c99a2d20fe71e5d
forevermango <raahima95@gmail.com>
2020-12-17 11:15:17 -0600

    added symbols to code

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template
 from patterns import patterns
+import yfinance as yf
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template('index.html', patterns=patterns)
+
+@app.route('/snapshot')
+def snapshot():
+    with open('datasets/companies.csv') as f:
+        symbols = f.read().splitlines()
+        print(symbols)
+
+    return {
+        'code': 'success'
+    }
