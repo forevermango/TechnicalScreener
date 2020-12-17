2c3f05b5de84f1b1d5e1003c7b8ed7d34cd8775d
forevermango <raahima95@gmail.com>
2020-12-17 11:51:31 -0600

    geting pcandlestick pattern outside

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from patterns import patterns
 import yfinance as yf
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
+    pattern = request.args.get('pattern', None)
+    if pattern:
+        print(pattern)
     return render_template('index.html', patterns=patterns)
 
 @app.route('/snapshot')
