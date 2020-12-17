7d58e5485359b98c819da138a16c5044a8bca30f
forevermango <raahima95@gmail.com>
2020-12-17 16:35:05 -0600

    added more style to the html and changed app.py

--- app.py
+++ app.py
@@ -9,42 +9,45 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    pattern = request.args.get('pattern', None)
+    current_pattern = request.args.get('pattern', False)
     stocks = {}
 
     with open('datasets/companies.csv') as f:
         for row in csv.reader(f):
             stocks[row[0]] = {'company': row[1]}
-    print(stocks)
+
     if pattern:
         datafiles = os.listdir('datasets/daily')
         for filename in datafiles:
             df = pd.read_csv('datasets/daily/{}'.format(filename))
-            #print(df)
             pattern_function = getattr(talib, pattern)
+            symbol = filename.split('.')[0]
             try:
                 result = pattern_function(df['Open'], df['High'], df['Low'], df['Close'])
                 last = result.tail(1).values[0]
-                if last < 0:
+                if last > 0:
                     stocks[symbol][pattern] = 'bullish'
                 elif last < 0:
                     stocks[symbol][pattern] = 'bearish'
                 else:                     
                     stocks[symbol][pattern] = None
 
-            except:
-                pass
+             except Exception as e:
+                print('failed on filename: ', filename)
 
-    return render_template('index.html', patterns=patterns, stocks=stocks, pattern=pattern)
+    return render_template('index.html', candlestick_patterns=candlestick_patterns, stocks=stocks, pattern=pattern)
+
 
 @app.route('/snapshot')
 def snapshot():
     with open('datasets/daily/companies.csv') as f:
         companies = f.read().splitlines()
-        for company in companies: 
-            symbol = company.split(',')[0]
-            df = yf.download(symbol, start="2020-01-01", end="2020-12-16")
-            df.to_csv('datasets/daily/{}.csv'.format(symbol))
+        for line in f:
+            if "," not in line:
+                continue
+            symbol = line.split(",")[0]
+            data = yf.download(symbol, start="2020-01-01", end="2020-08-01")
+            data.to_csv('datasets/daily/{}.csv'.format(symbol))
 
     return {
         'code': 'success'
